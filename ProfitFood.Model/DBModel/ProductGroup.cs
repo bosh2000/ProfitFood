fae27a653127459a973fe8d61896cdf2d524c8d7
Глупак Максим <kupel@inbox.ru>
2025-06-24 09:34:37 +1000

    Создание структуры проекта

---
+++ ProfitFood.Model/DBModel/ProductGroup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using ProfitFood.Model.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfitFood.Model.DBModel
+{
+    internal class ProductGroup
+    {
+
+        private ProductGroup(string name, string desc)
+        {
+            this.Name = name;
+            this.Description = desc;
+        }
+
+
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+
+
+        static Responce<ProductGroup> Create(string name, string desc,ILogger log) {
+            if (string.IsNullOrEmpty(name)) return new Responce<ProductGroup>(log)
+            {
+                IsSuccess = false,
+                ErrorMessage = "Не заполнено наименование"
+            };
+            return new Responce<ProductGroup>(log){ Data = new ProductGroup(name, desc) };
+        }
+    }
+}
---
+++ ProfitFood.Model/Infrastructure/Responce.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfitFood.Model.Infrastructure
+{
+    internal class Responce<T>
+    {
+        private readonly ILogger _logger;
+
+        public Responce(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public bool IsSuccess { get; set; } = true;
+        public string? ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+        public T? Data { get; set; }
+
+        public void Error(string? errorCode, string? errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+            _logger.LogError(errorCode, errorMessage);
+        }
+    }
+}
