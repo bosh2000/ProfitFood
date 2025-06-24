4edee534bab043100192777f694c0101baea5492
Глупак Максим <kupel@inbox.ru>
2025-06-24 10:26:44 +1000

    Добавление версии сборки (AutomaicVersion)

--- ProfitFood.Model/DBModel/Product.cs
+++ ProfitFood.Model/DBModel/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProfitFood.Model.DBModel
 {
-    internal class Product
+    public class Product
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
